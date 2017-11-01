ee7a9d4a5a6d527de4ee7545256e5040e96ca333
Rich Megginson <rmeggins@redhat.com>
2017-11-01 09:03:03 -0600

    modernize and equalize fluentd vs. rsyslog pipeline

--- openshift-test.sh
+++ openshift-test.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 set -euxo pipefail
 
@@ -9,17 +9,27 @@ USE_FLUENTD=${USE_FLUENTD:-true}
 USE_GDB=${USE_GDB:-false}
 ES_VER=${ES_VER:-2.4.4}
 DEBUG_FLUENTD=false
-# by default, if we're using journal, we will use it for both system messages
-# and container messages
-USE_JOURNAL_FOR_CONTAINERS=${USE_JOURNAL_FOR_CONTAINERS:-$USE_JOURNAL}
+# if true, use the journal for system messages/.operations
+# if false, use /var/log/messages
+USE_JOURNAL_FOR_SYSTEM=${USE_JOURNAL_FOR_SYSTEM:-true}
+# if true, use the journal for container messages
+# if false, use json file
+USE_JOURNAL_FOR_CONTAINERS=${USE_JOURNAL_FOR_CONTAINERS:-false}
+if [ $USE_JOURNAL_FOR_SYSTEM = true -o $USE_JOURNAL_FOR_CONTAINERS = true ] ; then
+    USE_JOURNAL=true
+else
+    USE_JOURNAL=false
+fi
 # so, something broke somewhere about using systemd-journal-remote on Fedora
 # to generate a journal that can be read on EL7 - it used to work (maybe
 # on F23?) but on F24 it does not work - so, have to run systemd-journal-remote
 # in a centos7 container to generate the messages.journal file in a format that
 # can be read by fluentd
 USE_CONTAINER_FOR_JOURNAL_FORMAT=${USE_CONTAINER_FOR_JOURNAL_FORMAT:-true}
-USE_JOURNAL=${USE_JOURNAL:-false}
-SKIP_MESSAGES_TEST=${SKIP_MESSAGES_TEST:-false}
+# this will grab every message stored in Elasticsearch and compare it against
+# the original written to file/journal - skip this if your message count
+# exceeds 9999
+SKIP_MESSAGES_TEST=${SKIP_MESSAGES_TEST:-true}
 
 if [ "$USE_JOURNAL" = "true" ] ; then
     if [ "${USE_CONTAINER_FOR_JOURNAL_FORMAT:-}" != true ] ; then
@@ -42,12 +52,13 @@ for id in `docker ps -qa` ; do
     fi
 done
 
-workdir=`mktemp -d`
+workdir=$( mktemp -d -p /var/tmp )
 mkdir -p $workdir
 confdir=$workdir/config
 datadir=$workdir/data
 mkdir -p $confdir
-mkdir -p $datadir/docker
+mkdir -p $datadir/containers
+mkdir -p $datadir/rsyslog
 sudo chown -R $USER $workdir
 sudo chcon -R unconfined_u:object_r:svirt_sandbox_file_t:s0 $workdir
 
@@ -64,6 +75,9 @@ if [ "$USE_JOURNAL" = "true" ] ; then
     rsyslog_bindmount="-v $datadir/journal:/var/log/journal"
 else
     systemlog=$datadir/messages
+fi
+if [ $USE_JOURNAL_FOR_CONTAINERS = false ] ; then
+    fluentd_docker_input=openshift-fluentd-json-file.conf
 fi
 
 cleanup() {
@@ -82,12 +96,12 @@ trap "exit" INT TERM
 trap "cleanup" EXIT
 
 wait_until_cmd() {
-    ii=$2
-    interval=${3:-10}
+    local ii=$2
+    local interval=${3:-10}
     while [ $ii -gt 0 ] ; do
         $1 && break
         sleep $interval
-        ii=`expr $ii - $interval`
+        ii=$( expr $ii - $interval )
     done
     if [ $ii -le 0 ] ; then
         return 1
@@ -108,20 +122,15 @@ curl_es() {
        http://${1}:9200/${2}*/${3}\?size=9000\&q=${4}:"${5}""${6:-}"
 }
 
-get_count_from_json() {
-    python -c 'import json, sys; print json.loads(sys.stdin.read())["count"]'
+curl_es_ext() {
+    local host=$1 ; shift
+    local url="$1"; shift
+    curl --connect-timeout 1 -s \
+       "http://${host}:9200${url}" "$@"
 }
 
-# return true if the actual count matches the expected count, false otherwise
-# $1 - es hostname
-# $2 - project name (e.g. logging, test, .operations, etc.)
-# $3 - field to search
-# $4 - search string
-# $5 - expected count
-test_count_expected() {
-    myfield=${myfield:-message}
-    nrecs=`curl_es $1 $2 _count $3 $4 | get_count_from_json`
-    test "$nrecs" = $5
+get_count_from_json() {
+    python -c 'import json, sys; print json.load(sys.stdin).get("count", 0)'
 }
 
 format_syslog_message() {
@@ -138,110 +147,129 @@ format_syslog_message() {
     fi
 }
 
-# get the date in utc Z format instead of regular -Ins format
-get_date() {
-    date -u +%FT%T.%NZ
+format_json_file() {
+    local full=$1
+    local nrecs=$2
+    local prefix=$3
+    local msgsize=$4
+    local startts=$( date -u +%s%N )
+    python -c 'import sys
+from datetime import datetime
+full = sys.argv[1]
+nrecs = int(sys.argv[2])
+width = len(sys.argv[2])
+prefix = sys.argv[3]
+msgsize = int(sys.argv[4])
+tsstr = sys.argv[5]
+ts = int(tsstr)
+
+if full == "full":
+  template = "{{{{\"log\":\"{prefix}-{{ii:0{width}d}} {{ii:0{msgsize}d}}\\n\",\"stream\":\"stdout\",\"time\":\"{{ts}}\",\"ident\":\"{prefix}\"}}}}\n".format(prefix=prefix, width=width, msgsize=msgsize)
+else:
+  template = "{prefix}-{{ii:0{width}d}} {{ii:0{msgsize}d}}\n".format(prefix=prefix, width=width, msgsize=msgsize)
+for ii in xrange(1, nrecs + 1):
+  tsstr = datetime.utcfromtimestamp(ts/1000000000.0).isoformat() + "000Z"
+  sys.stdout.write(template.format(ts=tsstr, ii=ii))
+  ts = ts + 1000
+' $full $nrecs $prefix $msgsize $startts
 }
 
-format_json_message() {
-    # $1 - full or short
-    # $2 - $NFMT
-    # $3 - $EXTRAFMT
-    # $4 - $prefix
-    # $5 - $ii
-    msg=`printf "%s-$2 $3" $4 $5 1`
-    if [ "$1" = "full" ] ; then
-        printf '{"log":"%s\\n","stream":"stdout","time":"%s"}\n' "$msg" "`get_date`"
-    else
-        printf "%s\n" "$msg"
-    fi
-}
+# create a journal which has N records - output is journalctl -o export format
+# suitable for piping into systemd-journal-remote
+# if nproj is given, also create N records per project
+format_journal() {
+    local nrecs=$1
+    local prefix=$2
+    local msgsize=$3
+    local needops=$4
+    local needapps=$5
+    local hn=$( hostname -s )
+    local startts=$( date -u +%s%6N )
+    python -c 'import sys
+nrecs = int(sys.argv[1])
+width = len(sys.argv[1])
+prefix = sys.argv[2]
+msgsize = int(sys.argv[3])
+needops = sys.argv[4].lower() == "true"
+needapps = sys.argv[5].lower() == "true"
+hn = sys.argv[6]
+tsstr = sys.argv[7]
+ts = int(tsstr)
+pid = sys.argv[8]
+if len(sys.argv) > 9:
+  nproj = int(sys.argv[9])
+  projwidth = len(sys.argv[9])
+  contprefix = sys.argv[10]
+  podprefix = sys.argv[11]
+  projprefix = sys.argv[12]
+  poduuid = sys.argv[13]
+  contfields = """CONTAINER_NAME=k8s_{contprefix}{{jj:0{projwidth}d}}.deadbeef_{podprefix}{{jj:0{projwidth}d}}_{projprefix}{{jj:0{projwidth}d}}_{poduuid}_abcdef01
+CONTAINER_ID={xx}
+CONTAINER_ID_FULL={yy}
+""".format(contprefix=contprefix,projwidth=projwidth,podprefix=podprefix,projprefix=projprefix,poduuid=poduuid,xx="1"*12,yy="1"*64)
+else:
+  nproj = 0
+  contfields = ""
 
-get_journal_timestamp() {
-    # microseconds since epoch
-    date -u +%s%6N
-}
-
-# journal -o export format for use with
-# /usr/lib/systemd/systemd-journal-remote - -o /path/to/log.journal
-# __CURSOR=s=f689c3b9b8dc4465acd22433bde89aed;i=71f60;b=0937011437e44850b3cb5a615345b50f;m=d3bafda0f;t=5330b9fab5ca6;x=244e23370340b59b
-# _SOURCE_REALTIME_TIMESTAMP=1431439843797660
-# __REALTIME_TIMESTAMP=1463499900017830
-# __MONOTONIC_TIMESTAMP=56835955215
-# _BOOT_ID=0937011437e44850b3cb5a615345b50f
-# _UID=1000
-# _GID=1000
-# _CAP_EFFECTIVE=0
-# _SYSTEMD_OWNER_UID=1000
-# _SYSTEMD_SLICE=user-1000.slice
-# _MACHINE_ID=68fe516b647f4d0fb5b0439d57b79344
-# _HOSTNAME=localhost.localdomain
-# _TRANSPORT=stdout
-# PRIORITY=4
-# _AUDIT_SESSION=1
-# _AUDIT_LOGINUID=1000
-# _SYSTEMD_CGROUP=/user.slice/user-1000.slice/session-1.scope
-# _SYSTEMD_SESSION=1
-# _SYSTEMD_UNIT=session-1.scope
-# _SELINUX_CONTEXT=unconfined_u:unconfined_r:unconfined_t:s0-s0:c0.c1023
-# SYSLOG_FACILITY=3
-# CODE_FILE=../src/core/manager.c
-# CODE_LINE=1761
-# CODE_FUNCTION=process_event
-# SYSLOG_IDENTIFIER=firefox.desktop
-# _COMM=firefox
-# _EXE=/usr/lib64/firefox/firefox
-# _CMDLINE=/usr/lib64/firefox/firefox -new-instance -P default
-# MESSAGE=Failed to open VDPAU backend libvdpau_va_gl.so: cannot open shared object file: No such file or directory
-# _PID=2685
-format_journal_message() {
-    # $1 - full or short
-    # $2 - $NFMT
-    # $3 - $EXTRAFMT
-    # $4 - $prefix
-    # $5 - $ii
-    # $6 - CONTAINER_NAME
-    # $7 - CONTAINER_ID
-    # $8 - CONTAINER_ID_FULL
-    fac=1
-    sev=2
-    msg=`printf "%s-$2 $3" $4 $5 1`
-    if [ "$1" = "full" ] ; then
-        ts=`get_journal_timestamp`
-        hn=`hostname -s`
-        tee -a /tmp/junk <<EOF
-_SOURCE_REALTIME_TIMESTAMP=$ts
-__REALTIME_TIMESTAMP=$ts
+template = """_SOURCE_REALTIME_TIMESTAMP={{ts}}
+__REALTIME_TIMESTAMP={{ts}}
 _BOOT_ID=0937011437e44850b3cb5a615345b50f
 _UID=1000
 _GID=1000
-_HOSTNAME=$hn
-SYSLOG_IDENTIFIER=$4
-SYSLOG_FACILITY=$fac
-_COMM=$4
-_PID=$$
-MESSAGE=$msg
+_HOSTNAME={hn}
+SYSLOG_IDENTIFIER={prefix}
+SYSLOG_FACILITY=1
+_COMM={prefix}
+_PID={pid}
 _TRANSPORT=stderr
 PRIORITY=3
-UNKNOWN1=1
-UNKNOWN2=2
-EOF
-        if [ -n "${6:-}" ] ; then
-            tee -a /tmp/junk <<EOF
-CONTAINER_NAME=$6
-CONTAINER_ID=$7
-CONTAINER_ID_FULL=$8
-EOF
-        fi
-        echo "" | tee -a /tmp/junk
-    else
-        echo "$msg"
-    fi
+""".format(prefix=prefix, hn=hn, width=width, pid=pid, contfields=contfields)
+
+template = template + """MESSAGE={prefix}-{{ii:0{width}d}} {msg:0{msgsize}d}
+""".format(prefix=prefix, width=width, msgsize=msgsize, msg=0)
+
+conttemplate = template + contfields
+
+for ii in xrange(1, nrecs + 1):
+  if needops:
+    sys.stdout.write(template.format(ts=ts, ii=ii) + "\n")
+    ts = ts + 1
+  if needapps:
+    for jj in xrange(1, nproj + 1):
+      sys.stdout.write(conttemplate.format(ts=ts, ii=ii, jj=jj) + "\n")
+      ts = ts + 1
+' $nrecs $prefix $msgsize $USE_JOURNAL_FOR_SYSTEM $USE_JOURNAL_FOR_CONTAINERS $hn $startts $$ ${NPROJECTS:-0} ${contprefix:-""} ${podprefix:-""} ${projprefix:-""} $( uuidgen )
+}
+
+format_external_project() {
+    local nrecs=$1
+    local prefix=$2
+    local msgsize=$3
+    local hn=$( hostname -s )
+    local startts=$( date -u +%s.%6N )
+    python -c 'import sys,json
+from datetime import datetime,timedelta
+nrecs = int(sys.argv[1])
+width = len(sys.argv[1])
+prefix = sys.argv[2]
+msgsize = int(sys.argv[3])
+hn = sys.argv[4]
+tsstr = sys.argv[5]
+ts = datetime.fromtimestamp(float(tsstr))
+usec = timedelta(microseconds=1)
+msgtmpl = "{prefix}-{{ii:0{width}d}} {msg:0{msgsize}d}".format(prefix=prefix, width=width, msgsize=msgsize, msg=0)
+hsh = {"hostname": hn, "level": "err", "ident": prefix}
+for ii in xrange(1, nrecs + 1):
+  hsh["@timestamp"] = ts.isoformat()+"+00:00"
+  hsh["message"] = msgtmpl.format(ii=ii)
+  sys.stdout.write(json.dumps(hsh, indent=None, separators=(",", ":")) + "\n")
+  ts = ts + usec
+' $nrecs $prefix $msgsize $hn $startts
 }
 
 # number of projects, number size, printf format
 NPROJECTS=${NPROJECTS:-10}
-NPSIZE=${NPSIZE:-2}
+NPSIZE=${NPSIZE:-$( echo $NPROJECTS | wc -c )}
 NPFMT=${NPFMT:-"%0${NPSIZE}d"}
 
 podprefix="this-is-pod-"
@@ -250,16 +278,6 @@ contprefix="this-is-container-"
 format_json_filename() {
     # $1 - $ii
     printf "%s${NPFMT}_%s${NPFMT}_%s${NPFMT}-%s.log\n" "$podprefix" $1 "$projprefix" $1 "$contprefix" $1 "`echo $1 | sha256sum | awk '{print $1}'`"
-}
-
-# CONTAINER_NAME=k8s_bob.94e110c7_bob-iq0d4_default_2d67916a-1eac-11e6-94ba-001c42e13e5d_8b4b7e3d
-# From this, we can extract:
-#    container name in pod: bob
-#    pod name: bob-iq0d4
-#    namespace: default
-#    pod uid: 2d67916a-1eac-11e6-94ba-001c42e13e5d
-get_journal_container_name() {
-    printf "k8s_%s${NPFMT}.deadbeef_%s${NPFMT}_%s${NPFMT}_%s_abcdef01\n" "$contprefix" $1 "$podprefix" $1 "$projprefix" $1 `uuidgen`
 }
 
 pushd ../docker-elasticsearch
@@ -278,28 +296,23 @@ ES_VER=$ES_VER DB_IN_CONTAINER=1 sh -x $testdir/../docker-elasticsearch/run-cont
 
 # number of messages per project
 NMESSAGES=${NMESSAGES:-10}
-# number size e.g. log base 10 of $NMESSAGES
-NSIZE=${NSIZE:-$( echo $NMESSAGES | wc -c )}
-# printf format for message number
-NFMT=${NFMT:-"%0${NSIZE}d"}
 # size of each message - number of bytes to write to each line of the logger file, not
 # the actual size of the JSON that is stored into ES
 MSGSIZE=${MSGSIZE:-200}
 
 ii=1
-prefix=`uuidgen`
-# need $MSGSIZE - (36 + "-" + $NSIZE + " ") bytes
-n=`expr $MSGSIZE - 36 - 1 - $NSIZE - 1`
-EXTRAFMT=${EXTRAFMT:-"%0${n}d"}
+prefix=$( uuidgen )
 if [ "$USE_JOURNAL" = "true" ] ; then
-    formatter=format_journal_message
+    formatter=format_journal
     if [ "${USE_CONTAINER_FOR_JOURNAL_FORMAT:-}" = true ] ; then
         sysfilter() {
             cat >> $datadir/journalinput.txt
         }
         postprocesssystemlog() {
             docker build -t viaq/journal-maker:latest journal-maker
-            docker run --privileged -u 0 -e INPUTFILE=/var/log/journalinput.txt -e OUTPUTFILE=/var/log/journal/messages.journal -v $datadir:/var/log viaq/journal-maker:latest
+            docker run --privileged -u 0 -e INPUTFILE=/var/log/journalinput.txt \
+                -e OUTPUTFILE=/var/log/journal/messages.journal \
+                -v $datadir:/var/log viaq/journal-maker:latest
             sudo chown -R ${USER}:${USER} $datadir/journal
         }
     else
@@ -310,7 +323,8 @@ if [ "$USE_JOURNAL" = "true" ] ; then
             :
         }
     fi
-else
+fi
+if [ $USE_JOURNAL_FOR_SYSTEM = false ] ; then
     formatter=format_syslog_message
     sysfilter() {
         cat >> $systemlog
@@ -319,29 +333,18 @@ else
         :
     }
 fi
-while [ $ii -le $NMESSAGES ] ; do
-    # direct to /var/log/messages format
-    $formatter full "$NFMT" "$EXTRAFMT" "$prefix" "$ii" | sysfilter
-    $formatter short "$NFMT" "$EXTRAFMT" "$prefix" "$ii" >> $orig
+$formatter $NMESSAGES $prefix $MSGSIZE $USE_JOURNAL_FOR_SYSTEM $USE_JOURNAL_FOR_CONTAINERS | sysfilter
+postprocesssystemlog
+
+if [ $USE_JOURNAL_FOR_CONTAINERS = false ] ; then
     jj=1
     while [ $jj -le $NPROJECTS ] ; do
-        if [ $USE_JOURNAL_FOR_CONTAINERS = false ] ; then
-            fn=`format_json_filename $jj`
-            format_json_message full "$NFMT" "$EXTRAFMT" "$prefix" "$ii" >> $datadir/docker/$fn
-            format_json_message short "$NFMT" "$EXTRAFMT" "$prefix" "$ii" >> $orig
-        else
-            CONTAINER_NAME=`get_journal_container_name $jj`
-            CONTAINER_ID_FULL=`echo $jj | sha256sum | awk '{print $1}'`
-            CONTAINER_ID=`echo $jj | sha256sum | awk '{print substr($1, 1, 12)}'`
-            $formatter full "$NFMT" "$EXTRAFMT" "$prefix" "$ii" $CONTAINER_NAME $CONTAINER_ID_FULL $CONTAINER_ID | sysfilter
-            $formatter short "$NFMT" "$EXTRAFMT" "$prefix" "$ii" $CONTAINER_NAME $CONTAINER_ID_FULL $CONTAINER_ID >> $orig
-        fi
-        jj=`expr $jj + 1`
+        fn=$( format_json_filename $jj )
+        format_json_file full $NMESSAGES $prefix $MSGSIZE > $datadir/containers/$fn
+        format_json_file short $NMESSAGES $prefix $MSGSIZE >> $orig
+        jj=$( expr $jj + 1 )
     done
-    ii=`expr $ii + 1`
-done
-
-postprocesssystemlog
+fi
 
 if [ "$USE_FLUENTD" = "true" ] ; then
     pushd ../docker-fluentd
@@ -351,6 +354,12 @@ if [ "$USE_FLUENTD" = "true" ] ; then
     #    cp openshift-fluentd.conf $confdir/fluent.conf
     cp -r openshift-fluentd/* $confdir
     cp $fluentd_syslog_input $confdir/configs.d/dynamic/input-syslog-default-syslog.conf
+    if [ $USE_JOURNAL_FOR_CONTAINERS = false ] ; then
+        cp $fluentd_docker_input $confdir/configs.d/dynamic/input-docker-default-docker.conf
+    fi
+    rm -f $confdir/configs.d/openshift/input-post-forward-mux.conf \
+        $confdir/configs.d/openshift/filter-pre-mux*.conf \
+        $confdir/configs.d/openshift/filter-post-mux*.conf \
     # run fluentd with the config dir mounted as /etc/fluent
     STARTTIME=$(date +%s)
     collectorid=`docker run -p 24220:24220 -p 5141:5141/udp -v $datadir:/var/log -v $confdir:/etc/fluent --link viaq-elasticsearch -e ES_HOST=viaq-elasticsearch -e OPS_HOST=viaq-elasticsearch -e ES_PORT=9200 -e OPS_PORT=9200 -e JOURNAL_SOURCE=/var/log/journal -e USE_JOURNAL=$USE_JOURNAL -e JOURNAL_READ_FROM_HEAD=true -e SYSLOG_LISTEN_PORT=5141 -e DEBUG_FLUENTD=${DEBUG_FLUENTD} -e ES_SCHEME=http -e OPS_SCHEME=http -d viaq/fluentd:latest`
@@ -365,21 +374,24 @@ else
     # run rsyslog with the config dir mounted as /etc/rsyslog.d
     STARTTIME=$(date +%s)
     if [ "$USE_GDB" != false ] ; then
-        docker run -p 5141:5141/udp $rsyslog_bindmount -v $datadir:/datadir --link viaq-elasticsearch -e ES_HOST=viaq-elasticsearch -e SYSLOG_LISTEN_PORT=5141 -it viaq/rsyslog-perf-test:latest gdb --args /usr/sbin/rsyslogd -n
+        docker run -p 5141:5141/udp $rsyslog_bindmount -v $datadir:/var/log --link viaq-elasticsearch -e ES_HOST=viaq-elasticsearch -e SYSLOG_LISTEN_PORT=5141 -it viaq/rsyslog-perf-test:latest gdb --args /usr/sbin/rsyslogd -n
     else
-        collectorid=`docker run -p 5141:5141/udp $rsyslog_bindmount -v $datadir:/datadir --link viaq-elasticsearch -e ES_HOST=viaq-elasticsearch -e SYSLOG_LISTEN_PORT=5141 -d viaq/rsyslog-perf-test:latest`
+        collectorid=`docker run -p 5141:5141/udp $rsyslog_bindmount -v $datadir:/var/log --link viaq-elasticsearch -e ES_HOST=viaq-elasticsearch -e SYSLOG_LISTEN_PORT=5141 -d viaq/rsyslog-perf-test:latest`
     fi
 fi
 
-count_ge_nmessages() {
-    curcount=`curl_es $myhost $myproject _count $myfield "$mymessage" | get_count_from_json`
-    echo count $curcount time $(date +%s)
-    test "${curcount:-0}" -ge $NMESSAGES
+test_count_expected() {
+    local curcount=`curl_es_ext $myhost "$myurl" -XPOST "$myqs" | get_count_from_json`
+    echo count $curcount time $( date +%s )
+    test "${curcount:-0}" -eq $NMESSAGES
 }
 
-echo waiting for $NMESSAGES messages in .operations and $NPROJECTS projects in elasticsearch
+max_wait_time=$( expr \( $NMESSAGES \* \( $NPROJECTS + 1 \) \) / 75 )
 
-myhost=localhost myproject=.operations myfield=systemd.u.SYSLOG_IDENTIFIER mymessage=$prefix wait_until_cmd count_ge_nmessages 30 1 || {
+echo waiting $max_wait_time for $NMESSAGES messages in .operations and $NPROJECTS projects in elasticsearch
+
+qs='{"query":{"term":{"ident":"'"${prefix}"'"}}}'
+myhost=localhost myurl="/.operations.*/_count" myqs="$qs" wait_until_cmd test_count_expected $max_wait_time 1 || {
     echo error: $NMESSAGES messages not found in .operations
     curl_es localhost .operations _search ident $prefix | python -mjson.tool
     exit 1
@@ -388,9 +400,9 @@ myhost=localhost myproject=.operations myfield=systemd.u.SYSLOG_IDENTIFIER mymes
 iii=1
 while [ $iii -le $NPROJECTS ] ; do
     myproject=`printf "%s${NPFMT}" $projprefix $iii`
-    myhost=localhost myproject=project.$myproject myfield=message mymessage=$prefix wait_until_cmd count_ge_nmessages 60 1 || {
+    myhost=localhost myurl="/project.$myproject.*/_count" myqs="$qs" wait_until_cmd test_count_expected $max_wait_time 1 || {
         echo error: $NMESSAGES messages not found in $myproject
-        curl_es localhost project.$myproject _search message $prefix | python -mjson.tool
+        curl_es localhost project.$myproject _search ident $prefix | python -mjson.tool
         exit 1
     }
     iii=`expr $iii + 1`
@@ -400,13 +412,13 @@ done
 # mark time
 MARKTIME=$(date +%s)
 
+echo duration `expr $MARKTIME - $STARTTIME`
+
 if [ "$SKIP_MESSAGES_TEST" = "true" ] ; then
     echo Skipping tests, code will terminate in 30 sec.
     sleep 30
     exit
 fi
-
-echo duration `expr $MARKTIME - $STARTTIME`
 
 # search ES and extract the messages
 #esmessages=`mktemp`
